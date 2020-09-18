d009f3dc45b36b99a71850779e1b54a6ec3a1a8e
Sara Álvarez Turrión <sara_sat93@hotmail.com>
2020-09-18 13:57:56 +0200

    terminado el drud

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/ClienteServiceImpl.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/ClienteServiceImpl.java
@@ -21,6 +21,27 @@ public class ClienteServiceImpl implements IClienteService{
 		return (List<Cliente>) clienteDao.findAll();
 	}
 
+	@Override
+	@Transactional
+	public Cliente save(Cliente cliente) {
+	
+		return clienteDao.save(cliente);
+	}
+
+	@Override
+	@Transactional
+	public void delete(Long id) {
+
+		clienteDao.deleteById(id);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Cliente findbyId(Long id) {
+	
+		return clienteDao.findById(id).orElseGet(null);
+	}
+
 	
 
 }
--- src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/IClienteService.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/IClienteService.java
@@ -7,5 +7,11 @@ import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
 public interface IClienteService {
 	
 	public List<Cliente> findAll();
+	
+	public Cliente save(Cliente cliente);
+	
+	public void delete(Long id);
+	
+	public Cliente findbyId(Long id);
 
 }
